d7b14ae7822b47a3cfc32f34279e9bd3424836b0
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-10 03:20:32 -0300

    feat: User List #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
-import { ListItem } from "react-native-elements";
+import { Alert, FlatList, Text, View } from "react-native";
+import { Button, Icon, ListItem } from "react-native-elements";
 import users from "../data/users";
 
 export default props => {
+
+    function confirmUserDeletion(user){
+        Alert.alert("Excluir usuário", "Deseja excluir o usuário?", [
+            {
+                text: "Sim",
+                onPress(){
+                    console.warn("Delete" + user.id)
+                }
+            },
+            {
+                text: "Não"
+            }
+        ])
+    }
+
+    function getActions(user){
+        return (
+            <>
+                <Button 
+                    onPress={() => props.navigation.navigate("UserForm", user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button 
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }){
         return <ListItem 
@@ -12,7 +43,8 @@ export default props => {
             title={user.name}
             subtitle={user.email}
             bottomDivider
-            onPress={() => props.navigation.navigate("UserForm")}
+            rightAvatar={getActions(user)}
+            onPress={() => props.navigation.navigate("UserForm", user)}
         />
     }
 
